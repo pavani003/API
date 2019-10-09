d94ae11e75d00863977df7a88e2b70582d381a79
Guttikonda <Guttikop@ES.AD.ADP.com>
2019-10-09 12:26:38 +0530

    handling promise rejection

--- app.js
+++ app.js
@@ -6,4 +6,23 @@ var api = require('./api.js');
 
 app.use('/api', api);
 
-app.listen(3000);
+var request = require("request");
+var options =
+ {
+  method: 'GET',
+  url: "https://api.yelp.com/v3/businesses/busZqvXX73idGQMLagMtpQ",
+  headers:
+         {
+         authorization:"Bearer YwVGsgo08WguXOmUQao5BdxlDq0Tek3XLft6NsdTSPvCTSa_uu9WxBmSI3zboviwnVMXBftoJ0KnEfIfKcQGv5In61Uaj7LRQKqITbR3Unrs_AP_RbziVd4fuAWbXXYx"
+         },
+ // body: '{}'
+ };
+ request(options, function (error, response, body){
+    if (error) {  
+         res.status(400).json({MESSAGE:MESSAGE})
+     }
+     else
+     console.log(response);
+    });
+
+//app.listen(3000);
